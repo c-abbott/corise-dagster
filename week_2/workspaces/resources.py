8f0067844bf9e1938843bcec295ff1c13451bb6b
Callum Abbott <cllmbbtt@gmail.com>
2022-12-03 10:12:24 +0000

    Added S3 resource

--- week_2/workspaces/resources.py
+++ week_2/workspaces/resources.py
@@ -90,9 +90,14 @@ def mock_s3_resource(context):
 
 
 @resource
-def s3_resource():
+def s3_resource(context) --> S3:
     """This resource defines a S3 client"""
-    pass
+    return S3(
+        bucket=context.resource_config["bucket"],
+        access_key=context.resource_config["access_key"],
+        secret_key=context.resource_config["secret_key"],
+        endpoint_url=context.resource_config["endpoint_url"],
+    )
 
 
 @resource
